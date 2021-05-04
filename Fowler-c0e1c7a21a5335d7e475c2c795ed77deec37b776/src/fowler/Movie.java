f445747655f40d0654165386f17b84e2b6b343cb
Marina Vollmer <mvollmer@allegion.com>
2021-05-04 18:57:27 +0200

    format code and add Generic Type

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/fowler/Movie.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/fowler/Movie.java
@@ -1,25 +1,57 @@
 package fowler;
+
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-    private String title;
-    private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
-        title = newtitle;
-        priceCode = newpriceCode;
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+
+	private final String title;
+	private int priceCode;
+
+	public Movie(String newtitle, int newpriceCode) {
+		title = newtitle;
+		priceCode = newpriceCode;
+	}
+
+	public int getPriceCode() {
+		return priceCode;
+	}
+
+	public void setPriceCode(int arg) {
+		priceCode = arg;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public double getCharge(int days) {
+		double result = 0;
+		switch (this.getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (days > 2)
+				result += (days - 2) * 1.5;
+			break;
+		
+		case Movie.NEW_RELEASE : 
+			result += days * 3;
+			break;
+		case Movie.CHILDRENS :
+			result += 1.5;
+			if (days > 3)
+				result += (days - 3) * 1.5;
+			break;
+		default:
+		    System.out.println("Sorry");
+		}
+		return result;
+	}
+
+	public Object getFrequentRenterPoints(int days) {
+		if (this.priceCode == Movie.NEW_RELEASE && days > 1)
+            return 2;
+        else
+            return 1;
     }
-    public int getPriceCode() {
-        return priceCode;
-    }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
-    }
-    public String getTitle (){
-        return title;
-    }
-	public Object getFrequentRenterPoints(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	};
 }
--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/fowler/Rental.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/fowler/Rental.java
@@ -1,19 +1,27 @@
 package fowler;
+
 public class Rental {
-    private Movie movie;
-    private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
-    }
-    public int getDaysRented() {
-        return daysRented;
-    }
-    public Movie getMovie() {
-        return movie;
-    }
-	public Object getFrequentRenterPoints() {
-		// TODO Auto-generated method stub
-		return null;
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie newmovie, int newdaysRented) {
+		movie = newmovie;
+		daysRented = newdaysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public double getCharge() {
+		return this.movie.getCharge(this.daysRented);
+	}
+
+	public int getFrequentRenterPoints() {
+		return (int) this.movie.getFrequentRenterPoints(this.daysRented);
 	}
 }
